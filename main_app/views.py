71c89fa6fd2739192eb13325e61ae60bb64a178c
Gabriela Cabrera <gaby.cabrera9@gmail.com>
2020-10-06 14:50:41 -0400

    create model linked to DB

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,9 @@
 from django.shortcuts import render
+from .models import Cookie
 
 # Create your views here.
 
 from django.http import HttpResponse
-
-class Cookie:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, main_flavor, description, prep_time):
-    self.name = name
-    self.main_flavor = main_flavor
-    self.description = description
-    self.prep_time = prep_time
-
-cookies = [
-    Cookie('Chocolate-Chip Cookies', 'Chocolate', 'Incredibly soft chocolate chip cookie', 1.5),
-    Cookie('Almond Extract Cookies', 'Almonds', 'Crunchy, flavorful almond cookie', 2),
-    Cookie('Sweet Raisin Cookies', 'Raisins', 'Low calorie raisin cookie', 1)
-]
 
 def home(request):
     return render(request, 'home.html')
@@ -24,4 +12,5 @@ def about(request):
     return render(request, 'about.html')
 
 def cookies_index(request):
+    cookies = Cookie.objects.all()
     return render(request, 'cookies/index.html', {'cookies': cookies})
