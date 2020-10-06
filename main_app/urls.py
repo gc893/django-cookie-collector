478c4db6aa6b3178096ab02b22b75d00c2ccc8e4
Gabriela Cabrera <gaby.cabrera9@gmail.com>
2020-10-06 15:05:59 -0400

    add details route and page

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('cookies/', views.cookies_index, name='index'),
+    path('cookies/<int:cookie_id>/', views.cookies_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -14,3 +14,7 @@ def about(request):
 def cookies_index(request):
     cookies = Cookie.objects.all()
     return render(request, 'cookies/index.html', {'cookies': cookies})
+
+def cookies_detail(request, cookie_id):
+  cookie = Cookie.objects.get(id=cookie_id)
+  return render(request, 'cookies/detail.html', { 'cookie': cookie })
