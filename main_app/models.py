a578dc52ffa7de848ef4cf477502507d588dc804
Gabriela Cabrera <gaby.cabrera9@gmail.com>
2020-10-06 17:01:00 -0400

    add cookie images

--- main_app/models.py
+++ main_app/models.py
@@ -7,6 +7,7 @@ class Cookie(models.Model):
     main_flavor = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     prep_time = models.IntegerField()
+    image_url = models.CharField(max_length=255, null=True)
 
     def __str__(self):
         return self.name
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # Cookie Collector
 ![](https://embed.widencdn.net/img/mccormick/u8pntu7ahp/2028x1141px/Vanilla_rich_chocolate_chip_cookies_004.jpg?crop=true&anchor=13,86&q=80&color=ffffffff&u=o2hyef)
-![](https://i.imgur.com/CfiDSfE.png)
+![](https://i.imgur.com/2dmfF39.png)
